ded76e12b938bb9d68c83efce61529c462dfbc0b
Maxim <Maksim23man@yandex.ru>
2017-11-12 18:58:14 +0700

    Initial commit 1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Учусь PHP</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<div class="ss">пилотка </div>
+
+<!-- todo Список задачь, не забудь про это-->
+
+</body>
+</html>
